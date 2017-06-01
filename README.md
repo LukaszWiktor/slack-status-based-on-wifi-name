77e2cd0a2f96f109f9a2a6f46b0abb19cc4f08e0
Craig Sketchley <craigsketchley@gmail.com>
2017-06-01 18:06:59 +1000

    Working on macos.

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ Set your status on Slack based on the WiFi network you are connected to.
 ## How to run it?
 
 You need to obtain a Slack token for your account from https://api.slack.com/custom-integrations/legacy-tokens.
-Copy and paste the token to [config.js](./config.js) or set it as an environment variable `SLACK_TOKEN`.
+Copy and paste the token into a file called `.env` or set it as an environment variable `SLACK_TOKEN`.
 
-    C:\slack-status-based-on-wifi-name>set SLACK_TOKEN=xoxp-...
+    set SLACK_TOKEN=xoxp-...
 
-You also need to have node.js installed to run the script. Currently it works on Windows only.
+You also need to have node.js installed to run the script. Currently it works on Windows and macOS only.
 
-    C:\slack-status-based-on-wifi-name>node app.js
-    
+    node app.js
+
 ## How does it work?
 
 The script checks periodically the WiFi network name (SSID) you are connected to and sets your status on Slack according to the mapping defined in [config.js](./config.js).
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const os = require('os');
 const axios = require("axios");
 const execSync = require("child_process").execSync;
 const querystring = require("querystring");
@@ -8,7 +10,16 @@ if (!config.slackToken) {
     process.exit(1);
 }
 
-function getWiFiName() {
+function getMacWiFiName() {
+    return execSync("/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I") // macos only
+            .toString()
+            .split("\n")
+            .filter(line => line.includes(" SSID: "))
+            .map(line => line.match(/: (.*)/)[1])
+            .find(ssid => true); // find first
+}
+
+function getWinWiFiName() {
     return execSync("netsh wlan show interfaces") // Windows only
             .toString()
             .split("\n")
@@ -30,11 +41,22 @@ function setSlackStatus(token, status) {
             console.log("Set Slack status API response: %j", response.data);
         })
         .catch(function(error) {
-           console.error("Set Slack status error: %s", error); 
+            console.error("Set Slack status error: %s", error);
         });
 }
 
-var wiFiName;
+const platform = os.platform();
+
+let wiFiName;
+let getWiFiName;
+// Get appropriate function for platform
+switch (platform) {
+  case 'darwin':
+    getWiFiName = getMacWiFiName;
+    break;
+  default:
+    getWiFiName = getWinWiFiName;
+}
 
 setInterval(function() {
     const newWiFiName = getWiFiName();
@@ -43,12 +65,12 @@ setInterval(function() {
     }
     wiFiName = newWiFiName;
     console.log("Connected WiFi SSID: %s", wiFiName);
-    
+
     const status = config.statusByWiFiName[wiFiName];
     if (!status) {
         console.log("Status not specified for WiFi: %s", wiFiName);
         return;
     }
     console.log("Setting Slack status to: %j", status);
-    setSlackStatus(config.slackToken, status); 
+    setSlackStatus(config.slackToken, status);
 }, config.updateInterval);
